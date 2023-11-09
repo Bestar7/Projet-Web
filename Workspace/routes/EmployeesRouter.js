39bac7e016cc24c09b68341a37908feb1c14713c
Joachim Bastin Local <joachim.bastin@gmail.com>
2023-11-09 16:39:18 +0100

    cleanup

--- Workspace/routes/EmployeesRouter.js
+++ Workspace/routes/EmployeesRouter.js
@@ -1,12 +1,10 @@
 const express = require("express")
 const { sequelize, DataTypes} = require("../util/database");
-//const { DataTypes } = require("sequelize");
 //const Customers = require("../models/Customers")(sequelize, DataTypes);
 const Employees = require("../models/Employees")(sequelize, DataTypes);
 
 var router = express.Router();
 
-//const user = await Employees.
 // Routes. These blocks should be placed in different files under "routes/" directory
 // but let's keep this simple.
 router.get("/", (request, response) => {
@@ -19,24 +17,6 @@ router.get("/", (request, response) => {
   }).catch((err) => {
       console.log("$$$$$$ findAll()\n  "+err);
   })
-  /*
-  sequelize.authenticate().then((val)=>console.log(val));
-  //const ok = await this.mo
-  sequelize.sync()//({force: true}) // pour mettre DAO en DB
-  .then((result) => {
-    //console.log(result.config);
-    //return Employees.findAll()
-    */
-    /*
-    Employees.findAll().then((people) => {
-      response
-        .type("text")
-        .send(people)
-      console.log(people)
-      return people
-    }).catch((err) => {
-      console.log("$$$$$$ findAll()\n  "+err);
-    })*/
 })
 
 module.exports = router;
